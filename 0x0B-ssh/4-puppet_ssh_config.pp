874418a71655801213cf8cfce74f270f95aef2f9
xica369 <761@holbertonschool.com>
2019-08-07 05:35:31 +0000

    T4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Execute a command
 exec { 'configuration-ssh':
-  command => '/bin/echo -e "IdentityFile ~/.ssh/holberton\nPasswordAuthentication no" >> /etc/ssh/sshd_config',
+  command => '/bin/echo -e "IdentityFile ~/.ssh/holberton\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
   path    => '/usr/bin';
 }
