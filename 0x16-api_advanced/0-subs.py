726596b8fdde9bb11f56b8080b24598aa4216163
xica369 <761@holbertonschool.com>
2019-09-19 22:04:15 +0000

    T0: How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" function that queries the Reddit API """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ returns the number of subscribers for a given subreddit.
+    If an invalid subreddit is given, the function should return 0."""
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return (response.get("data").get("subscribers"))
+    return (0)
