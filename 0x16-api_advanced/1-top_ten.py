9a9c8798665229d47ccec9a455137dc448a7f45f
xica369 <761@holbertonschool.com>
2019-09-20 00:37:24 +0000

    T1: add else

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,13 +6,14 @@ import requests
 def top_ten(subreddit):
     "prints the titles of the first 10 hot posts listed for a given subreddit"
     headers = {'User-Agent': 'xica369'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'limit': 10}
     response = requests.get(url, headers=headers, allow_redirects=False,
                             params=parameters)
 
     if response.status_code == 200:
-        titles = response.json().get('data').get('children')
-        for title in titles:
-            print(title.get('data').get('title'))
-    print(None)
+        titles_ = response.json().get('data').get('children')
+        for title_ in titles_:
+            print(title_.get('data').get('title'))
+    else:
+        print(None)
