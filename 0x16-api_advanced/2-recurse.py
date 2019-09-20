491235576fc1462b985dff9c7895e6c6e923907c
xica369 <761@holbertonschool.com>
2019-09-20 00:19:11 +0000

    T2: add global variable

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ recursive function that queries the Reddit API """
 import requests
+after = None
 
 
 def recurse(subreddit, hot_list=[]):
@@ -8,18 +9,21 @@ def recurse(subreddit, hot_list=[]):
     subreddit.
     If no results are found for the given subreddit,
     the function should return None. """
+    global after
     headers = {'User-Agent': 'xica369'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    parameters = {'after': 'after', 'limit': 100}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
     response = requests.get(url, headers=headers, allow_redirects=False,
                             params=parameters)
 
     if response.status_code == 200:
         next_ = response.json().get('data').get('after')
         if next_ is not None:
-            recurse(subreddit, hot_list, next_)
-        titles_list = response.json().get('data').get('children')
-        for title in titles_list:
-            hot_list.append(title.get('data').get('title'))
-        return (hot_list)
-    return (None)
+            after = next_
+            recurse(subreddit, hot_list)
+        list_titles = response.json().get('data').get('children')
+        for title_ in list_titles:
+            hot_list.append(title_.get('data').get('title'))
+        return hot_list
+    else:
+        return (None)
