45aad228fcb913142cb55e2ad0cd6678c7354571
xica369 <761@holbertonschool.com>
2019-09-23 01:42:10 +0000

    T3: Count it [fixed]

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -23,8 +23,8 @@ def count_words(subreddit, word_list):
             count_words(subreddit, word_list)
         list_objs = response.json().get('data').get('children')
         for obj in list_objs:
+            title_list = (obj.get('data').get('title')).split()
             for word in word_list:
-                title_list = (title_.get('data').get('title')).split()
                 if word in title_list:
                     count_dic[word] = count_dict.keys() + 1
         sort_dic = sorted(count_dic.items(), key=operator.itemgetter(1))
