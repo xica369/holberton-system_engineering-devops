235635890215159b549bb512013c81520f17c43a
xica369 <761@holbertonschool.com>
2019-10-10 23:08:04 +0000

    T1: puppet-lint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,10 @@
 # configuration the limits to open files of user holberton
 exec { 'change-os-configuration-for-holberton-user':
-  command  => "sed -i 's/hard nofile 5/hard nofile 5000/g' /etc/security/limits.conf && sed -i 's/soft nofile 4/soft nofile 4000/g' /etc/security/limits.conf",
+  command  => "sed -i 's/hard nofile 5/hard nofile 5000/g' /etc/security/limits.conf",
+  provider => shell;
+}
+
+exec {'change-limits':
+  command  => "sed -i 's/soft nofile 4/soft nofile 4000/g' /etc/security/limits.conf",
   provider => shell;
 }
